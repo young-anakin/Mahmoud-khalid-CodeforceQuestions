0857a72baa90ca73179cf8cc1258752c07b72336
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2025-02-23 23:00:17 +0300

    update

--- BinarySearch/O_Really_Big_Numbers.py
+++ BinarySearch/O_Really_Big_Numbers.py
@@ -1,21 +1,27 @@
 n, k = list(map(int, input().split(" ")))
 def binarySearch(l, r, k):
-    while l < r:
+    while l <= r:
         md = (l+r)//2
         val = 0
         for i in str(md):
             val += int(i)
         
         # val = int(val)
+        # print(md-val, md)
         if md - val > k:
-            r -=1
-        elif md - val < k:
-            l +=1
+            r = md - 1
+        elif md - val < k:  
+            l = md+1
         else:
-            return x
-        print(md, l, r)
+            # print("yaaaa")
+            return l
+        # print(md, l, r)
     
-    return r
+    return l
         
 x = binarySearch(1, n, k)
-print(x)
+if x > n:
+    print(0)
+else:
+    print(abs(x-n) + 1)
+# print(x)
