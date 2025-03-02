58d707a85a2cdae81d7b5d0c17c3de00100c75aa
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2025-03-02 20:44:29 +0300

    r:

--- BinarySearch/A_New_World_New_Me_New_Array.py
+++ BinarySearch/A_New_World_New_Me_New_Array.py
@@ -1,19 +1,32 @@
-t = int(input())
-for _ in range(t):
-    n, k, p = map(int, input().split())
+n, x = list(map(int, input().split(" ")))
+arr = list(map(int, input().split(" ")))
+both = []
+import bisect
+valid = False
+arr2 = list(sorted(arr))
+first, second = 0, 0
+for i in range(len(arr)):
+    mn = bisect.bisect_left(arr, x-arr2[i])
+    if mn < len(arr2):
+        if arr2[mn] + arr2[i] == x:
+            if mn == i:
+                if arr2.count(arr2[mn]) > 1:
+                    xx = []
+                    for i in range(len(arr)):
+                        if arr[i] == arr2[mn]:
+                            xx.append(i)
+                    print(xx[0]+1, xx[1]+1)
+                    exit()
+                else:
+                    continue
+            first, second = arr2[i], arr2[mn]
+            valid = True
+            break
 
-    if abs(k) > n * p:
-        print(-1)
-        continue
 
-    if k == 0:
-        print(0)
-        continue
-
-
-    min_moves = (abs(k) + p - 1) // p  
-
-    if min_moves > n:
-        print(-1)
-    else:
-        print(min_moves)
+if not valid:
+    print(-1)
+else:
+    a, b = arr.index(first), arr.index(second)
+    print(a+1, b+1)
+    
--- BinarySearch/A_Sum_of_Two_Values.py
+++ BinarySearch/A_Sum_of_Two_Values.py
@@ -1,8 +1,32 @@
 n, x = list(map(int, input().split(" ")))
 arr = list(map(int, input().split(" ")))
-
+both = []
 import bisect
+valid = False
+arr2 = list(sorted(arr))
+first, second = 0, 0
+for i in range(len(arr)):
+    mn = bisect.bisect_left(arr, x-arr2[i])
+    if mn < len(arr2):
+        if arr2[mn] + arr2[i] == x:
+            if mn == i:
+                if arr2.count(arr2[mn]) > 1:
+                    xx = []
+                    for i in range(len(arr)):
+                        if arr[i] == arr2[mn]:
+                            xx.append(i)
+                    print(xx[0]+1, xx[1]+1)
+                    exit()
+                else:
+                    continue
+            first, second = arr2[i], arr2[mn]
+            valid = True
+            break
 
-for i in arr:
-    rem = bisect.bisect_left(arr, (x-i))
-    print
+
+if not valid:
+    print(-1)
+else:
+    a, b = arr.index(first), arr.index(second)
+    print(a+1, b+1)
+    
